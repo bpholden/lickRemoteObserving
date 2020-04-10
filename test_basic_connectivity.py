cde71adcb3341bee5312a2481cd8d1f0d7f89e0c
Bradford Holden <brad.p.holden@gmail.com>
2020-04-09 17:03:38 -0700

    new tests (and removed unused tests)

--- test_basic_connectivity.py
+++ test_basic_connectivity.py
@@ -1,8 +1,8 @@
+import os
 import logging
 import getpass
 from lick_vnc_launcher import create_logger, LickVncLauncher, create_parser
 import pytest
-
 
 # create lvl object
 create_logger()
@@ -12,26 +12,26 @@ lvl.log_system_info()
 lvl.args = create_parser()
 lvl.get_config()
 lvl.check_config()
-if lvl.config.get('nosshkey', False) is True:
-    vnc_account = lvl.args.account
-    lvl.vnc_password = getpass.getpass(f"\nPassword for user {vnc_account}: ")
 
 servers_and_results = [('shimmy', 'shimmy.ucolick.org'),
                        ('noir', 'noir.ucolick.org')
                            ]
 
-def test_firewall_authentication():
-    lvl.is_authenticated = False
-    if lvl.do_authenticate:
-        lvl.firewall_pass = getpass.getpass(f"\nPassword for firewall authentication: ")
-        lvl.is_authenticated = lvl.authenticate(lvl.firewall_pass)
-        assert lvl.is_authenticated is True
-
+def test_vncviewer():
+    lvl.log.info('Testing config file: vncviewer')
+    vncviewer = lvl.config.get('vncviewer', None)
+    if vncviewer in [None, '', 'vncviewer']:
+        # the line below will throw and error if which fails
+        vncviewer = subprocess.check_output(['which', 'vncviewer'])
+    if vncviewer != 'open':
+        assert os.path.exists(vncviewer)
+    
 
 def test_ssh_key():
-    if lvl.config.get('nosshkey', False) is not True:
-        lvl.validate_ssh_key()
-        assert lvl.ssh_key_valid is True
+    lvl.log.info('Testing config file: ssh_pkey')
+    lvl.tel = 'shane'
+    lvl.validate_ssh_key()
+    assert lvl.ssh_key_valid is True
 
 
 @pytest.mark.parametrize("server,result", servers_and_results)
