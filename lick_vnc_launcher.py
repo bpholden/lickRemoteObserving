2d5f032ebf57f9194ede606998639a37f0c8405a
Bradford Holden <brad.p.holden@gmail.com>
2020-07-12 13:01:11 -0700

    error?

--- lick_vnc_launcher.py
+++ lick_vnc_launcher.py
@@ -1158,7 +1158,7 @@ class LickVncLauncher(object):
         try:
              xpdyinfo = subprocess.run('xdpyinfo', stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE, timeout=5)
-        except subprocess.TimeoutError as e:
+        except subprocess.TimeoutExpired as e:
              # If xpdyinfo fails just log and keep going
              self.log.debug('xpdyinfo failed')
              self.log.debug(e)
