0e3178f9752e30aef89d1e0cfcceafc1033ea70a
Bradford Holden <brad.p.holden@gmail.com>
2020-10-19 15:44:21 -0700

    new version

--- lick_vnc_launcher.py
+++ lick_vnc_launcher.py
@@ -25,7 +25,7 @@ import yaml
 
 import soundplay
 
-__version__ = '1.10'
+__version__ = '1.11'
 
 ##-------------------------------------------------------------------------
 ## Start from command line
